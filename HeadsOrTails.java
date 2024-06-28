e7ffcdc418c5912e81c85d92457d9620656b8754
okuramogumogu <oukuramoto@icloud.com>
2024-06-28 17:52:58 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsOrTails {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Tossing a coin...");
+
+        int headsCount = 0, tailsCount = 0;
+        for (int round = 1; round <= 3; round++) {
+            int result = random.nextInt(2); // 0 for Heads, 1 for Tails
+            if (result == 0) {
+                System.out.println("Round " + round + ": Heads");
+                headsCount++;
+            } else {
+                System.out.println("Round " + round + ": Tails");
+                tailsCount++;
+            }
+        }
+
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+
+        scanner.close();
+    }
+}
